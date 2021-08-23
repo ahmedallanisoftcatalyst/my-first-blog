e6a881a9290bee5acf7b3985f959afc1441841b5
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2021-08-23 15:42:16 +0100

    environ variables

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
+
+env = environ.Env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,10 +22,13 @@ TIME_ZONE = 'Africa/Tunis'
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-i+(idnhw_@3^n$*r5(1963hva)@_&e!d1fs6$roik#$c&c1#h'
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="syRv8cHUFelftIRrBzSOqihPRFBvZMtiX5sUpn1PcobCv9k4Rxm6doXicKAHOSer",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -127,7 +133,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'your gmail'
-EMAIL_HOST_PASSWORD = 'your gmail password'
+EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = False
