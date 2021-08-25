ee0e0aa54d017ae83255d114bf5a706234c3f41c
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2021-08-25 17:39:45 +0100

    origin post form

--- blog/forms.py
+++ blog/forms.py
@@ -3,18 +3,17 @@ from django.core.validators import RegexValidator
 
 from .models import Post
 
-email_softcatalyst_validator = RegexValidator(
-    regex='@softcatalyst\.com$',
-    message='Email is invalid. The email should be a softcatalyst email',
-    code='invalid_email',
-)
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
-    title = forms.EmailField(label='Your email', max_length=100, required=True,validators=[email_softcatalyst_validator])
 
+email_softcatalyst_validator = RegexValidator(
+    regex='@softcatalyst\.com$',
+    message='Email is invalid. The email should be a softcatalyst email',
+    code='invalid_email',
+)
 class FeedbackForm(forms.Form):
     name = forms.CharField(label='Your name', max_length=100, required=True)
     email = forms.EmailField(label='Your email', max_length=100, required=True,validators=[email_softcatalyst_validator])
